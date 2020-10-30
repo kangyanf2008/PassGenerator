24e4f055eacaac6924da8da1b1637ddb150f4282
kangyanf <kangyanf2008@163.com>
2020-10-30 18:37:32 +0800

    修改默认服务端key

--- src/config/constants.go
+++ src/config/constants.go
@@ -2,7 +2,7 @@ package config
 
 var(
 	KEY string = "cGVuc2Vlc0AyMDIwMTAyOQ=="
-	ServerKEY string = "20201030@!admin!"
+	ServerKEY string = "K8Ff3KY4gSjGstf%"
 	SEPARATE_CHAR string = "_"
 	LF byte = 10
 	CR byte = 13
