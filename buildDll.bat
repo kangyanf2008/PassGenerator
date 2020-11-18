1d94cae2306bc13b57a0570a556eca02a9243e81
kangyanf <kangyanf2008@163.com>
2020-11-18 13:31:03 +0800

    修改编译脚本

--- buildDll.bat
+++ buildDll.bat
@@ -11,6 +11,8 @@ go build -p 4 -buildmode=c-archive -o ../lib/pg.a .
 ::生成静太库
 ::go build -p 4 -buildmode=c-archive -o ../lib/pg.lib .
 cd ../lib/
-gcc pg.def pg.a -shared -lwinmm -lWs2_32 -o pg.dll -Wl,--out-implib,pg.dll.a
+
+gcc pg.def pg.a -shared -lwinmm -lWs2_32 -lpwKey -lWibuCm64  -L./ -o pg.dll -Wl,--out-implib,pg.dll.a
+
 lib /def:pg.def /name:pg.dll /out:pg.lib /MACHINE:X86
 cd ..
---
+++ lib/pwKey.h
@@ -0,0 +1,14 @@
+#ifndef _pwKey
+#define _pwKey
+#define DEFAULT_PRODUCT_CODE 4300
+#define MUL_FIRM_CODE 103391
+/**
+读取拓展数据
+*/
+int getExtDataKey(unsigned long ProductCode, unsigned long mulFirmCode, unsigned char data[1024], unsigned int *ilen);
+/*
+读取隐密数据
+*/
+int getSecretDataKey(unsigned long ProductCode, unsigned long mulFirmCode, unsigned char data[1024], unsigned int *ilen);
+
+#endif
